98589b4b8d5cc3ff1dd211d4e2a64bfb59a42ac9
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2023-09-08 13:20:04 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert('Hello world')
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let number_1 = 5
+// let number_2 = 10
+
+// alert(`A soma de ${number_1} e ${number_2} é ${number_1 + number_2}`)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// let isNumber = 5
+
+// if (typeof isNumber === "number") {
+//     alert("É um número")
+// } else {
+//     alert("Não é um número")
+// }
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let isString = "Fernando"
+
+// if (typeof isString === "string") {
+//     alert("É uma string")
+// } else {
+//     alert("Não é uma string")
+// }
